ec3d8a755855e22a659385d43adc849897879c8e
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-18 13:36:54 +0530

    [Rishi]AddingIntoLinkedlist

---
+++ DataStruchers/DataStruchers/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStruchers
+{
+   public class Node
+    {
+        public int data;
+        public Node next;
+    }
+}
